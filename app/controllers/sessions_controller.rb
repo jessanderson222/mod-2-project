03ed66082d3dc0cb537acfe9da20946407a576bc
cfernando415 <ca.fernando415@gmail.com>
2018-12-05 10:14:04 -0500

    sessions working, need logout feature

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+  def create
+    # byebug
+    if params[:session][:user_name].empty? || params[:session][:password].empty?
+      flash[:errors] = "Username / Password cannot be blank!!!"
+      redirect_to login_path
+    else
+      @user = User.find_by(user_name: params[:session][:user_name])
+      if @user || @user.authenthicate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      end
+    end
+  end
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
    resources :categories, only:[:show]
    resources :ads, only:[:show, :new, :create]
    resources :users, only:[:show, :new, :create]
+   get "/login", to: "sessions#new"
+   post "/login", to: "sessions#create"
+   delete "/logout", to: "sessions#destroy"
   root "categories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
