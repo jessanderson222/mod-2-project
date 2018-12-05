6a2efca6c075c5dd73374c6106a5ea0cfe4a04dc
jessanderson222 <jessanderson222@gmail.com>
2018-12-05 17:00:29 -0500

    going to try and make a search bar

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :ads
+  has_many :ads, dependent: :destroy
   has_many :renters, :class_name => "User", through: :ads
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,11 @@
+
 User.create(user_name: "nottrevor", password_digest: "thisisnottrevor", email: "nottrevor@email.com", location: "Brooklyn")
+
 Category.create(name: 'Home Goods')
+Category.create(name: "Clothing, Shoes, & Jewelry")
+Category.create(name: "Books")
+Category.create(name: "Electronics")
+Category.create(name: "Beauty & Health")
+Category.create(name: "Toys, Kids, & Baby")
+Category.create(name: "Sports & Outdoors")
+Category.create(name: "Automotive")
