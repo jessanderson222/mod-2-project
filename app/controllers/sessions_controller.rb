c004f384182110603cf0d3018a737103d49921fc
cfernando415 <ca.fernando415@gmail.com>
2018-12-05 15:31:41 -0500

    login + logout feature complete, need to work on authorization on commenting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,14 @@ class SessionsController < ApplicationController
       redirect_to login_path
     else
       @user = User.find_by(user_name: params[:session][:user_name])
-      if @user || @user.authenthicate(params[:session][:password])
+      if @user && @user.authenticate(params[:session][:password])
         session[:user_id] = @user.id
         redirect_to @user
       end
     end
   end
   def destroy
+    session.delete :user_id
+    redirect_to root_path
   end
 end
